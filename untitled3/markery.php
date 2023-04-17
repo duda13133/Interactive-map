840477259eca6e02c2474760be835e0d68522614
Kacper Furman <84005621+duda13133@users.noreply.github.com>
2023-04-17 10:02:41 +0200

    Add files via upload

---
+++ untitled3/markery.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Export to PHP Array plugin for PHPMyAdmin
+ * @version 5.2.0
+ */
+
+/**
+ * Database `test`
+ */
+
+/* `test`.`markery` */
+$markery = array(
+  array('id' => '1','text' => 'Instytut Inzynierii Technicznej'),
+  array('id' => '2','text' => 'Biblioteka')
+);
